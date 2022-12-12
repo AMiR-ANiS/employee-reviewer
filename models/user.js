11a9323697efb9f61684e11ae368b51749bbcb8c
Amir Anis <amir.anis.rkl@gmail.com>
2022-12-13 02:31:52 +0530

    create user model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ['admin', 'employee'],
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
