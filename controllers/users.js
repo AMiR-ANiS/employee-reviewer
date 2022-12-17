4a658ca7c91f4ec0d15cb32fa884ae98efbdc1f4
Amir Anis <amir.anis.rkl@gmail.com>
2022-12-17 15:10:05 +0530

    setup route and controller for registering a user

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.signUp = (req, res) => {
   return res.render('user_sign_up', {
     title: 'Employee Reviewer | Sign Up'
@@ -8,4 +10,58 @@ module.exports.signIn = (req, res) => {
   return res.render('user_sign_in', {
     title: 'Employee Reviewer | Sign In'
   });
+};
+
+module.exports.createUser = async (req, res) => {
+  try {
+    if (req.body.name.length === 0) {
+      req.flash('error', 'Username cannot be empty!');
+      return res.redirect('back');
+    }
+
+    if (req.body.email.length === 0) {
+      req.flash('error', 'Email cannot be empty!');
+      return res.redirect('back');
+    }
+
+    if (req.body.password.length === 0) {
+      req.flash('error', 'Password cannot be empty!');
+      return res.redirect('back');
+    }
+
+    if (req.body.password !== req.body.confirm_password) {
+      req.flash('error', 'Password and confirm password should match!');
+      return res.redirect('back');
+    }
+
+    if (!req.body.user_type) {
+      req.flash('error', 'Please specify user type!');
+      return res.redirect('back');
+    }
+
+    let user = await User.findOne({
+      email: req.body.email
+    });
+
+    if (user) {
+      req.flash('error', 'User with that email already exists!');
+      return res.redirect('back');
+    } else {
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        type: req.body.user_type
+      });
+      req.flash(
+        'success',
+        'User registered successfully!, Please log in to continue!'
+      );
+      return res.redirect('/users/sign-in');
+    }
+  } catch (err) {
+    req.flash('error', 'Error in registering user!');
+    console.log(err);
+    return res.redirect('back');
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const usersController = require('../controllers/users');
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
+router.post('/create-user', usersController.createUser);
 
 module.exports = router;
